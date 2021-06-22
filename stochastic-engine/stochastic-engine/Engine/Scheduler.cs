d694e42c2262614711d9ab16d55d235ebabb3710
Fernando Pascoal <fernando.pascoal42@gmail.com>
2021-06-21 23:10:25 -0300

    Adicionar Resource, Scheduler e iniciar componentes da PetriNet.

---
+++ stochastic-engine/stochastic-engine/Engine/Scheduler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stochastic_engine.Engine
+{
+    public class Scheduler
+    {
+        public double Time { get; }
+    }
+}
---
+++ stochastic-engine/stochastic-engine/Models/Resource.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stochastic_engine.Models
+{
+    public class Resource
+    {
+        private string name;
+        public Guid Id { get; set; }
+        private int quantity;
+
+        public Resource(string name, int quantity, Guid id)
+        {
+            this.name = name;
+            this.quantity = quantity;
+            Id = id;
+        }
+
+        public bool Allocate(int quantity)
+        {
+            if (this.quantity >= quantity)
+            {
+                this.quantity -= quantity;
+                return true;
+            }
+            return false;
+        }
+
+        public void Release(int quantity)
+        {
+            this.quantity += quantity;
+        }
+    }
+}
