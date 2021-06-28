388675a301889d9b6487c2a51c90169c67a354e4
christianmlima <christianmellolima@outlook.com>
2021-06-27 23:06:22 -0300

    adicionado criador de petri

--- stochastic-engine/PetriNet/PetriNet.cs
+++ stochastic-engine/PetriNet/PetriNet.cs
@@ -16,7 +16,7 @@ namespace PetriNetProject
         public string DataString { get; set; } = "";
         public List<List<string>> Log { get; set; } = new List<List<string>>();
 
-        public PetriNet(string filename)
+        private PetriNet()
         {
             
         }
@@ -42,7 +42,7 @@ namespace PetriNetProject
         public Lugar GetLugarByName(string nome) => Lugares.FirstOrDefault(x => x.Nome == nome);
         public void RemoverLugar(int id) => Lugares.RemoveAll(x => x.Id == id);
         public void AddTransicao(int id, string nome) => Transicoes.Add(new Transicao(id, nome, 0));
-        public Transicao GetTransicao(int id) => Transicoes.FirstOrDefault(x => x.Id == id);
+        public Transicao GetTransicaoById(int id) => Transicoes.FirstOrDefault(x => x.Id == id);
         public Transicao GetTransicaoByName(string nome) => Transicoes.FirstOrDefault(x => x.Nome == nome);
         public void RemoverTransicao(int id) => Transicoes.RemoveAll(x => x.Id == id);
         public bool GetTransicaoIsAtiva(int id) => Transicoes.FirstOrDefault(x => x.Id == id).IsAtiva();
@@ -190,6 +190,53 @@ namespace PetriNetProject
             }
 
             DataString = DataString + dados + "\n";
+        }
+
+        public PetriNet CriarPetriNet()
+        {
+            var petri = new PetriNet();
+
+            petri.AddLugar(1, "GarçomLivre");
+            petri.AddLugar(2, "SubstituirCaixa");
+            petri.AddLugar(3, "PedidoPronto");
+            petri.AddLugar(4, "ClienteVaiSentar");
+            petri.AddLugar(5, "GarçomNoCaixa");
+            petri.AddLugar(6, "LevandoPedido");
+            petri.AddLugar(7, "HigienizandoMesa");
+            petri.AddLugar(8, "AtendenteVoltou");
+            petri.AddLugar(9, "PedidoEntregue");
+            petri.AddLugar(10, "MesaHigienizada");
+
+            petri.AddTransicao(1, "T1");
+            petri.AddTransicao(2, "T2");
+            petri.AddTransicao(3, "T3");
+            petri.AddTransicao(4, "T4");
+            petri.AddTransicao(5, "T5");
+            petri.AddTransicao(6, "T6");
+
+            petri.AddArco(petri.GetLugarById(1), petri.GetTransicaoById(1), 1, true, "normal", "A1", 1);
+            petri.AddArco(petri.GetLugarById(1), petri.GetTransicaoById(2), 1, true, "normal", "A2", 2);
+            petri.AddArco(petri.GetLugarById(1), petri.GetTransicaoById(3), 1, true, "normal", "A3", 3);
+            petri.AddArco(petri.GetLugarById(1), petri.GetTransicaoById(4), 1, false, "normal", "A4", 4);
+            petri.AddArco(petri.GetLugarById(1), petri.GetTransicaoById(5), 1, false, "normal", "A5", 5);
+            petri.AddArco(petri.GetLugarById(1), petri.GetTransicaoById(6), 1, false, "normal", "A6", 6);
+
+            petri.AddArco(petri.GetLugarById(2), petri.GetTransicaoById(1), 1, true, "normal", "A7", 7);
+            petri.AddArco(petri.GetLugarById(3), petri.GetTransicaoById(2), 1, true, "normal", "A8", 8);
+            petri.AddArco(petri.GetLugarById(4), petri.GetTransicaoById(3), 1, true, "normal", "A9", 9);
+
+            petri.AddArco(petri.GetLugarById(5), petri.GetTransicaoById(1), 1, false, "normal", "A10", 10);
+            petri.AddArco(petri.GetLugarById(5), petri.GetTransicaoById(4), 1, true, "normal", "A11", 11);
+            petri.AddArco(petri.GetLugarById(6), petri.GetTransicaoById(2), 1, false, "normal", "A12", 12);
+            petri.AddArco(petri.GetLugarById(6), petri.GetTransicaoById(5), 1, true, "normal", "A13", 13);
+            petri.AddArco(petri.GetLugarById(7), petri.GetTransicaoById(3), 1, false, "normal", "A14", 14);
+            petri.AddArco(petri.GetLugarById(7), petri.GetTransicaoById(6), 1, true, "normal", "A15", 15);
+
+            petri.AddArco(petri.GetLugarById(8), petri.GetTransicaoById(4), 1, true, "normal", "A16", 16);
+            petri.AddArco(petri.GetLugarById(9), petri.GetTransicaoById(5), 1, true, "normal", "A17", 17);
+            petri.AddArco(petri.GetLugarById(10), petri.GetTransicaoById(6), 1, true, "normal", "A18", 18);
+
+            return petri;
         }
     }
 }
